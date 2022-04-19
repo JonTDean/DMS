317cca5c83aaca4357cc396c35f9a54f34273e1a
Jonathan Dean <Jon@myishas-mbp.myfiosgateway.com>
2022-04-19 15:53:04 -0400

    Creating the parser system now

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ def read_file(filename):
 	with open(f'data/{filename}.txt', 'r') as f:
 		return f.read()
 	
-csv = read_file('comma');
-psv = read_file('pipe');
-ssv = read_file('space');
+psv = read_file('pipe').split('\n');
+csv = read_file('comma').split('\n');
+ssv = read_file('space').split('\n');
 
-print(csv);
-print(psv);
-print(ssv);
+# Start on PSV first beacuse its first in the file
+
+# Start on CSV
+
+# Start on SSV Start on the SSV last because 
+# we want to call the ssv parser when 
+# neither tabs nor commas are present
+
