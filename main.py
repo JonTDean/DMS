00ddce7015b5f1840084a4c29c85acfc209399ad
Jonathan Dean <Jon@myishas-mbp.myfiosgateway.com>
2022-04-19 15:34:00 -0400

    Created reader, next up is extracting

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 def read_file(filename):
-	with open('data/{filename}.txt', 'r') as f:
+	with open(f'data/{filename}.txt', 'r') as f:
 		return f.read()
+	
+csv = read_file('comma');
+psv = read_file('pipe');
+ssv = read_file('space');
+
+print(csv);
+print(psv);
+print(ssv);
